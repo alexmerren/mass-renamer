8ed64d9cedb72913390866f64339354453b49c66
Alex Merren <alexandermerren@gmail.com>
2020-07-05 22:28:23 +0100

    Updated massReame.cpp

--- massRename/massRename.cpp
+++ massRename/massRename.cpp
@@ -1,5 +1,5 @@
-#include <iostream> 
 #include <vector> 
+#include <iostream> 
 #include <chrono> 
 #include <cstdio>
 #include <cstring>
@@ -22,16 +22,16 @@ void renameFiles(std::vector<std::string> inputs, std::vector<std::string> outpu
 
 int main(int argc, char* argv[])
 {
-    int start = 1;
-    bool verbose = false;
-    
-    if ( argc == 1 )
+    if (argc == 1)
     {
         std::printf("Please enter a file name.\n");
         return 1;
     }
 
-    if ( (strcmp(argv[1],"-v")) == 0 ) 
+    int start = 1;
+    bool verbose = false;
+    
+    if ((strcmp(argv[1],"-v")) == 0) 
     {
         start = 2;
         verbose = true;
@@ -43,75 +43,61 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-std::vector<std::string> listFiles(std::string filesString) 
+std::vector<std::string> listFiles(std::string t_filesString) 
 {
 	// This is the array of all the files we are returning.
 	std::vector<std::string> filesArr; 
-	std::string file,delimiter; 
-	delimiter = ","; 
+	std::string file; 
+	std::string delimiter = ","; 
 	int pos = 0;
 
-	// This makes the integer pos into the location of where the first delimiter is
-	// from the start.  It continues to do this until it hits the end of the string.
-	// std::string::npos is a fancy way of doing that.
-	while ( ( pos = filesString.find( delimiter )) != std::string::npos ) 
+	while ((pos = t_filesString.find(delimiter)) != std::string::npos) 
 	{
-		// This string is the substring between commas, or a file that we want.	
-		file = filesString.substr( 0,pos ); 
-	
-        // Add the current file to the filesArr vector.
-		filesArr.push_back( file );
-	
-        // Remove the substring that we found and put into file, 
-		// and the comma that is next to it.
-		filesString.erase( 0, pos + delimiter.length() ); 
+		file = t_filesString.substr(0,pos); 
+		filesArr.push_back(file);
+		t_filesString.erase(0, pos + delimiter.length()); 
 	}
-	filesArr.push_back( filesString ); 
+	filesArr.push_back(t_filesString); 
 	return filesArr;
 }
 
-void renameFiles(std::vector<std::string> inputs, std::vector<std::string> outputs, bool print)
+void renameFiles(std::vector<std::string> t_inputs, std::vector<std::string> t_outputs, bool t_print)
 {
 	try
-	{
+    {
+        // Get the first element in the t_inputs and t_outputs vectors.
+		auto inputsBegin = t_inputs.cbegin(); 
+		auto outputsBegin = t_outputs.cbegin(); 
 		// This starts the timing clock.
 		auto start = std::chrono::high_resolution_clock::now(); 
-		
-        // Create const char* for conversion from string.
-		const char* inputsBeginChar; 
-		const char* outputsBeginChar;
-		
-        // Get the first element in the inputs and outputs vectors.
-		auto inputsBegin = inputs.cbegin(); 
-		auto outputsBegin = outputs.cbegin(); 
-		
-        // This for loop goes through the input and outputs vectors, and renames them.
-		for (; inputsBegin != inputs.end() and outputsBegin != outputs.end(); inputsBegin++, outputsBegin++ ) 
+        // This for loop goes through the input and t_outputs vectors, and renames them.
+		for (; inputsBegin != t_inputs.end() and outputsBegin != t_outputs.end(); inputsBegin++, outputsBegin++ ) 
 		{
 			// Convert std::string to const char*
-			inputsBeginChar = inputsBegin->c_str(); 
-		    outputsBeginChar = outputsBegin->c_str(); 
-			
-            if ( rename( inputsBeginChar, outputsBeginChar ) != 0 )
+			const char* inputsBeginChar = inputsBegin->c_str(); 
+		    const char* outputsBeginChar = outputsBegin->c_str(); 
+            try 
             {
-				perror( "Error renaming file" );
-			}
-            else if ( print )
+                std::rename(inputsBeginChar, outputsBeginChar);
+				if (t_print)
+                {
+                    std::printf("%s -> %s\n", inputsBeginChar, outputsBeginChar);  
+                }
+            }
+            catch (const std::exception& e)
             {
-				printf( "%s -> %s\n", inputsBeginChar, outputsBeginChar ); 
-	    	}
+                std::perror("");
+            }
         }
-		// This ends the timing clock.
 		auto finish = std::chrono::high_resolution_clock::now(); 
 		std::chrono::duration<double> elapsed = finish - start; 
-        
-        if ( print ) 
+        if (t_print) 
         {
-		    printf( "Renaming took %f seconds.\n", elapsed.count() ); 
+		    std::printf( "Renaming took %f seconds.\n", elapsed.count() ); 
         }
-	} 
+    }
     catch (const std::exception& e) 
     {	
-		printf("There was an error in the program.\n");
+		std::printf("");
 	}
 }
